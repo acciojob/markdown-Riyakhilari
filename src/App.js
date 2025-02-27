34aae6b2c680f3ff71488d271dcf9d5be11891ec
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-27 20:23:54 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarkdownEditor from "./components/MarkdownEditor"; // Corrected import
+
+function App() {
+  return (
+    <div>
+      <h1>Markdown Editor</h1>
+      <MarkdownEditor />
+    </div>
+  );
+}
+
+export default App;
