84d9c2e731b32c574f8c31200ac1335258897d58
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-03 22:44:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import MarkdownEditor from "./components/MarkdownEditor"; // Corrected import
+import MarkdownEditor from "./Appli"; // Corrected import
 
 function App() {
   return (
